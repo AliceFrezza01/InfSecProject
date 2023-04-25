e13e92bd4c0baa82a344667ff7a7515e31b66679
Alice.Frezza <alice.frezza@stud-inf.unibz.it>
2023-04-25 19:18:42 +0200

    putting the php about the login session in another page

--- landingpage.php
+++ landingpage.php
@@ -4,28 +4,7 @@
 <?php
 
     include ('connect.php');
-
-    if(!isset($_SESSION['loginsession'])){
-        header('location: login.php');
-    }
-    $userid = $_SESSION['loginsession'];
-    $userqueryresult = $con->query("SELECT * FROM user WHERE id = '$userid'");
-    if($userqueryresult->num_rows != 1){
-        //redirect also on login page because the userid does not exist or exists multiple times
-        header('location: login.php');
-    }
-
-    $user = $userqueryresult->fetch_assoc();
-
-    //function for the logout button
-    if (isset($_POST['logout'])) {
-        session_destroy();
-        ?>
-        <script>
-            window.location.replace("login.php");
-        </script>
-        <?php
-    }
+    include ('authentificationUser.php');
 
     $lowerStringInserted = "";
     //function for the search button
