e2784bb33abaf52667295c530363e1bd4c177757
Linda <LindaMaria.Pircher@stud-inf.unibz.it>
2023-04-25 16:25:46 +0200

    add mainfunctions.php

---
+++ mainfunctions.php
@@ -0,0 +1,12 @@
+<?php
+
+#Console Log Function
+function console_log($output, $with_script_tags = true){
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
+    if ($with_script_tags) { $js_code = '<script>' . $js_code . '</script>'; } echo $js_code; }
+
+#Input Function
+function input($data) {
+    $data = trim($data); $data = stripslashes($data); $data = htmlspecialchars($data); return $data;
+}
+?>
