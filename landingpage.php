9c19f55a598e9c6004a0d9a3e9fb82875832c25f
Alice.Frezza <alice.frezza@stud-inf.unibz.it>
2023-05-01 09:05:55 +0200

    move my (alice) css to styles.css

--- landingpage.php
+++ landingpage.php
@@ -31,41 +31,12 @@
         <title>Landing Page</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <style>
-        
-                /* Nav Bar Css */
-                .topnav {
-                background-color: #333;
-                overflow: hidden;
-                }
-
-                .topnav a {
-                float: left;
-                color: #f2f2f2;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-                }
-
-                .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-                }
-
-                .topnav a.active {
-                background-color: blue;
-                color: white;
-                }
-
-                .headerHello {
-                    background-color: gray;
-                }
-
                  /* Css for other */
                 .container-fluid {
                     border-style: solid;
